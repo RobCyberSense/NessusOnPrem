07f6e1f388a6c23e989a59d2c68a50d11ec6c2ae
RobCyberSense <131620561+RobCyberSense@users.noreply.github.com>
2023-04-23 22:28:36 +0100

    Add files via upload

---
+++ Configure-Firewall.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$allowedIP
+)
+
+# Get the Windows Management Instrumentation (WMI) inbound rule
+$wmiRule = Get-NetFirewallRule -DisplayName "Windows Management Instrumentation (WMI)"
+
+# If the WMI rule exists, add the IP address restriction
+if ($wmiRule -ne $null) {
+    $wmiRule | Set-NetFirewallRule -RemoteAddress $allowedIP
+}
+# If the WMI rule doesn't exist, create it with the IP address restriction
+else {
+    New-NetFirewallRule -DisplayName "Windows Management Instrumentation (WMI)" -Direction Inbound -Protocol TCP -LocalPort 135 -Program "System" -Action Allow -RemoteAddress $allowedIP
+}
+
+# Get the File and Printer Sharing inbound rule
+$filePrintRule = Get-NetFirewallRule -DisplayName "File and Printer Sharing"
+
+# If the File and Printer Sharing rule exists, add the IP address restriction
+if ($filePrintRule -ne $null) {
+    $filePrintRule | Set-NetFirewallRule -RemoteAddress $allowedIP
+}
+# If the File and Printer Sharing rule doesn't exist, create it with the IP address restriction
+else {
+    New-NetFirewallRule -DisplayName "File and Printer Sharing" -Direction Inbound -Protocol TCP -LocalPort 139,445 -Program "%SystemRoot%\system32\svchost.exe" -Service "LanmanServer" -Action Allow -RemoteAddress $allowedIP
+}
+
+# Set the Remote Registry service startup type to Manual
+Set-Service -Name RemoteRegistry -StartupType Manual
+
+# Add the LocalAccountTokenFilterPolicy registry key with value 1
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$registryName = "LocalAccountTokenFilterPolicy"
+$registryValue = 1
+New-ItemProperty -Path $registryPath -Name $registryName -Value $registryValue -PropertyType DWORD -Force
+
+#example: .\script.ps1 -allowedIP 192.168.0.247
---
+++ Revert-Firewall.ps1
@@ -0,0 +1,21 @@
+﻿# Remove the Windows Management Instrumentation (WMI) inbound rule
+$wmiRule = Get-NetFirewallRule -DisplayName "Windows Management Instrumentation (WMI)"
+if ($wmiRule -ne $null) {
+    Remove-NetFirewallRule -DisplayName "Windows Management Instrumentation (WMI)"
+}
+
+# Remove the File and Printer Sharing inbound rule
+$filePrintRule = Get-NetFirewallRule -DisplayName "File and Printer Sharing"
+if ($filePrintRule -ne $null) {
+    Remove-NetFirewallRule -DisplayName "File and Printer Sharing"
+}
+
+# Set the Remote Registry service startup type to Automatic
+Set-Service -Name RemoteRegistry -StartupType Disabled
+
+# Remove the LocalAccountTokenFilterPolicy registry key
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$registryName = "LocalAccountTokenFilterPolicy"
+if (Test-Path "$registryPath\$registryName") {
+    Remove-ItemProperty -Path $registryPath -Name $registryName
+}
